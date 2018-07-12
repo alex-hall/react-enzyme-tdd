0b234a21329bfc8ab7ef8719cc94b0840588fcbf
Alex Hall <me@jalexhall.com>
2018-07-12 13:07:31 -0400

    Let’s create the main test to drive out login functionality
    So this process will be both iterative from the angle of making source
    Code changes as well as test changes. We’ll keep bouncing back and
    Forth when the compiler/test suite tells us to do so!

--- src/components/Landing.test.js
+++ src/components/Landing.test.js
@@ -8,7 +8,7 @@ beforeEach(() => {
     component = shallow(<Landing/>)
 })
 
-describe('basic functionality', () => {
+describe('basic component layout and functionality', () => {
 
     it('shouldnt explode', () => {
         expect(component.exists()).toBeTruthy()
@@ -24,5 +24,14 @@ describe('login functionality', () => {
     it('should provide a username and password input box', () => {
         expect(component.find('[data-username-field]').exists()).toBeTruthy()
         expect(component.find('[data-password-field]').exists()).toBeTruthy()
+    })
+
+    it('should log the user in when they submit their credentials', () => {
+        component.find('[data-username-field]').simulate('change', {target: {value: "alex-hall"}})
+        component.find('[data-password-field]').simulate('change', {target: {value: 'SECRET PASSWORD'}})
+
+        component.find('[data-login-submit]').simulate('click')
+        
+        expect(component.find('[data-user-details]').text()).toEqual('Welcome Alex Hall, last logged in 01-01-1900')
     })
 })
