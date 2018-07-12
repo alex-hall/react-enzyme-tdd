674389e65edee39b707b27daac3417d50d13cd85
Alex Hall <me@jalexhall.com>
2018-07-12 12:15:23 -0400

    Let’s add login functionality!
    Notice we moved the beforeEach to a higher scope so multiple describe
    Blocks could inherit the component for free. 
    
    Also notice the bracket syntax on lines 25 and 26. These are selectors
    That describe data attributes, which we’ll be leveraging very heavily
    To describe unique components on the page. 
    
    Why data attributes vs classes or IDs? The answer is that both IDs and
    Classes could get refactored based on your stylesheet, but data 
    Attributes are somewhat reserved for data purposes (not that you can’t
    Write a css selector that looks for a data attribute, it’s just not 
    Great practice). 
    
    Finally `.toEqual(true)` and `.toBeTruthy()` are equivalent in this
    Use case. One again the goal of tests is to be readable and I personally
    find this a little easier to read.

--- src/components/Landing.test.js
+++ src/components/Landing.test.js
@@ -2,20 +2,27 @@ import React from "react";
 import Landing from "./Landing";
 import {shallow} from "enzyme";
 
+let component
+
+beforeEach(() => {
+    component = shallow(<Landing/>)
+})
 
 describe('basic functionality', () => {
-    let component
-    
-    beforeEach(() => {
-        component = shallow(<Landing/>)
-    })
 
     it('shouldnt explode', () => {
-        expect(component.exists()).toEqual(true)
+        expect(component.exists()).toBeTruthy()
     })
 
     it('should display a welcome message', () => {
         expect(component.find('h1').text()).toEqual('Welcome to the landing page!')
     })
+})
 
+describe('login functionality', () => {
+
+    it('should provide a username and password input box', () => {
+        expect(component.find('[data-username-field]').exists()).toBeTruthy()
+        expect(component.find('[data-password-field]').exists()).toBeTruthy()
+    })
 })
