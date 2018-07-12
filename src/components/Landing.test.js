a1ca8cb4a8efed8286bb0c225f8e10c5656b7bc4
Alex Hall <me@jalexhall.com>
2018-07-12 11:53:38 -0400

    Now to drive out some functionality
    Keep in mind the point of writing tests isn’t to describe *code*, but 
    Rather _functionality_. The reason for this is so you can write your
    Tests without having to worry about how you’re going to implement 
    something. It also gives you the freedom to refactor in the future 
    Without having to change tests. 
    
    Basic tests will likely be somewhat descriptive of code, but more 
    Complex tests should really focus on functionality. 
    
    Let’s write a simple test to kick us off.

--- src/components/Landing.test.js
+++ src/components/Landing.test.js
@@ -9,4 +9,11 @@ describe('basic functionality', () => {
 
         expect(component.exists()).toEqual(true)
     })
+
+    it('should display a welcome message', () => {
+        const component = shallow(<Landing/>)
+
+        expect(component.find('h1').text()).toEqual('Welcome to the landing page!')
+    })
+
 })
