950e77317531567f6e2c029be08db7be5b7ec4da
Alex Hall <me@jalexhall.com>
2018-07-12 11:18:22 -0400

    Let’s write our first failing test!
    The purpose of this initial test is to drive out the need to pull
    In react-router-dom. I’m familiar with this API so I had a good idea
    Of what to write initially, but their (documentation)[https://reacttraining.com/react-router/web/guides/quick-start] is pretty good also. 
    
    Basically what this test is saying is that on the default route “/“, 
    We’d expect the router to render the landing page.
    
    Let’s run `yarn test` to figure out what we need to do next.

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,12 @@ it('renders without crashing', () => {
 
     expect(component.exists()).toEqual(true)
 });
+
+it('renders the landing page component by default', () => {
+    const component = shallow(<App/>)
+
+    const defaultRoute = component.find(Route);
+
+    expect(defaultRoute.props().path).toEqual('/')
+    expect(defaultRoute.props().component).toEqual(Landing)
+})
